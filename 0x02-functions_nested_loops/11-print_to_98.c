f1dd9e2b336ac4087237e39b8d265d4524e03151
Matlala Mpho <mphotebogomatlala@gmail.com>
2023-03-04 06:42:53 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,6 @@
  */
 void print_to_98(int n)
 {
-
 	int i, j;
 
 	if (n <= 98)
@@ -21,10 +20,11 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
-		for (j - n; j >= 98; j--)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			if (j !-98)
-				printf("%d, ", j)
+			if (j != 98)
+				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
 		}
